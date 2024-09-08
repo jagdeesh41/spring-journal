51c025f34d7ff153976606efc5ca9460bc627fc2
jagdeesh41 <pepjaggu@gmail.com>
2024-09-08 18:54:20 +0530

    chore: using ResponseEntity<> in the controller

--- src/main/java/com/bot/journal/controller/JournalEntryController.java
+++ src/main/java/com/bot/journal/controller/JournalEntryController.java
@@ -4,6 +4,8 @@ import com.bot.journal.entity.JournalEntry;
 import com.bot.journal.model.JournalRequest;
 import com.bot.journal.service.JournalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +16,16 @@ import java.util.List;
 public class JournalEntryController {
     @Autowired
     private JournalService journalService;
-
     @GetMapping
-    public List<JournalEntry> getAll()
+    public ResponseEntity<List<JournalEntry>> getAll()
     {
-        return journalService.getAllJournals();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(this.journalService.getAllJournals());
     }
     @PostMapping
-    public void addJournal(@RequestBody JournalRequest journalRequest)
+    public  void addJournal(@RequestBody JournalRequest journalRequest)
     {
         journalService.addJournalEntry(journalRequest);
-
+        ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
